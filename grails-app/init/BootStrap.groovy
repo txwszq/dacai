e65e3ec6d649dffe15d58e5e83148f078b6bbea4
Michael <michael@Michaels-MacBook-Pro.local>
2016-12-17 23:50:24 +0800

    a

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -8,7 +8,12 @@ class BootStrap {
 
     def init = { servletContext ->
         def discuzs = Discuz.findAllByShoutingStatus(DiscuzShoutingStatus.NORMAL)
+
+
         discuzs.each {discuz ->
+            if (!discuz.maxId) {
+                discuzService.getUid(discuz)
+            }
             Shouting s = new Shouting(discuzService, discuz)
             Thread thread = new Thread(s)
             thread.start()
